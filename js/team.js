2028ec2fe8b24a1c33a4b4872c2a202cf5497759
ali32 <ali.xudiyev99@mail.ru>
2021-11-17 13:08:22 +0400

    21.0120.130

--- js/team.js
+++ js/team.js
@@ -1,4 +1,18 @@
 $(".menu-bar").click(function(){
-    $(".nav-li-bar").slideToggle()
+    $(".nav-li-bar").stop().slideToggle()
     
 })
+
+window.onload=function(){
+    setTimeout(()=>{
+    document.querySelector(".loader").classList.add("d-none")
+     document.body.style="overflow-y:scroll"
+    },1000)
+}
+window.onscroll=function(){
+
+    if(scrollY>120){
+    ("#header")
+
+    }
+}
